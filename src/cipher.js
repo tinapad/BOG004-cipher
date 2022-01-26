c5f45c61b07685304bf214eff77d391a1bb3db7a
tinapad <tinapad21@gmail.com>
2022-01-25 23:03:21 -0500

    FINAL

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,37 @@
 const cipher = {
-  
-  // ...
+  encode: function(offset, str) {
+    if (typeof offset !== "number" || typeof str !== "string"){
+      throw new TypeError ("Error!")
+    }
+    var aux = "";
+    for (var i = 0; i <str.length; i++){
+      if(str.charCodeAt(i)>=65&&str.charCodeAt(i)<=90){
+          aux+= String.fromCharCode((str.charCodeAt(i)+offset-65)%26+65)    
+      }else if(str.charCodeAt(i)>=97&&str.charCodeAt(i)<=122){
+        aux+= String.fromCharCode((str.charCodeAt(i)+offset-97)%26+97)
+      }else{  
+          aux = "No se admiten caracteres fuera del abecedario ingles"
+      }
+    } 
+    return aux;
+  },
+  decode: function(offset, string) {
+    if (typeof offset !== "number" || typeof string !== "string") { 
+      throw new TypeError ("Error!")
+    }
+    var aux = "";
+    for (var i = 0; i <string.length; i++){
+      if(string.charCodeAt(i)>=65&&string.charCodeAt(i)<=90){
+        aux+=String.fromCharCode((string.charCodeAt(i)-offset-90)%26+90)
+      }else  if(string.charCodeAt(i)>=97&&string.charCodeAt(i)<=122){
+        aux+=String.fromCharCode((string.charCodeAt(i)-offset-122)%26+122)
+      
+   }else{
+        aux = "No se admiten caracteres fuera del abecedario ingles "
+      }
+    }
+    return aux;
+  },
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import cipher from './cipher.js';
+const mensaje = document.getElementById("texto");
+const desplazamiento = document.getElementById("numero");
+const general = document.getElementById("general");
+const mensajeCodificar = document.getElementById("codificado");
+const mensajeDecodificar = document.getElementById("decodificado");
+const codificar = document.getElementById("btn1");
+const decodificar = document.getElementById("btn2");
 
-console.log(cipher);
+codificar.addEventListener("click", () => { 
+    const texto = mensaje.value;
+    const dz = parseInt(desplazamiento.value);
+    if (texto == ""){
+        general.innerHTML = "Ingresa texto en la casilla"
+    }else if (dz <=0 ){
+        general.innerHTML = "Ingresar un desplazamiento mayor a 0"
+    }else{
+        mensajeCodificar.innerHTML = cipher.encode(dz,texto);
+    }
+});
+decodificar.addEventListener("click", () => {
+    const texto = mensaje.value;
+    const dz = parseInt(desplazamiento.value);
+    if (texto == ""){
+        general.innerHTML = "Ingresa texto en la casilla"
+    }else if (dz <=0 ){
+        general.innerHTML = "Ingresar un desplazamiento mayor a 0"
+    }else{
+        mensajeDecodificar.innerHTML = cipher.decode(dz,texto);
+    }
+});
